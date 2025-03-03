5e38f36b0edcf3470a349aea7c27e7ad214388d5
David Okulski <32730627+DavidOkulski@users.noreply.github.com>
2025-03-03 12:46:03 -0800

    Added empoyee check to username

--- usernameHandler.js
+++ usernameHandler.js
@@ -1,22 +1,55 @@
 const axios = require("axios");
+const { keycloakForSiebel } = require("./keycloak.js");
+
+async function isUsernameValid(username) {
+    try {
+        const siebelApiUrl = process.env.SIEBEL_ICM_API_EMPLOYEE_URL;
+        const grant = await keycloakForSiebel.grantManager.obtainFromClientCredentials();
+        const response = await axios.get(siebelApiUrl, {
+            params: {
+                excludeEmptyFieldsInResponse: "true",
+                searchspec: `([Login Name] LIKE '${username}')`,
+                PageSize: "1",
+                ViewMode: "Catalog",
+            },
+            headers: {
+                Authorization: `Bearer ${grant.access_token.token}`,
+                "X-ICM-TrustedUsername": username,
+            },
+        });
+        return response.data?.items?.["Login Name"] === username;
+
+    } catch (error) {
+        if (error.response) {
+            console.error(`Siebel API Error (${error.response.status}): ${error.response.data?.ERROR || "Unknown error"}`);
+            throw new Error(`${error.response.status}`);
+        }
+        console.error("Error connecting to Siebel API:", error);
+        throw new Error("500 - Internal Server Error");
+    }
+}
 
 async function getUsername(userToken) {
     if (!userToken) {
-        console.warn("No user token provided. Using default.");
-        return process.env.TRUSTED_USERNAME;
+        console.warn("No user token provided.");
+        return null;
     }
 
     try {
-        const userInfoResponse = await axios.get(process.env.USERNAME_SERVERURL,
-            {
-                headers: {
-                    Authorization: `Bearer ${userToken}`,
-                },
-            }
-        );
-
+        const userInfoResponse = await axios.get(process.env.USERNAME_SERVERURL, {
+            headers: {
+                Authorization: `Bearer ${userToken}`,
+            },
+        });
         if (userInfoResponse.data && userInfoResponse.data.idir_username) {
-            return userInfoResponse.data.idir_username;
+            const username = userInfoResponse.data.idir_username;
+
+            try {
+                const isValid = await isUsernameValid(username);
+                return isValid ? username : null;
+            } catch (error) {
+                return error.message;
+            }
         } else {
             console.warn("Username not found.");
         }
@@ -24,8 +57,7 @@ async function getUsername(userToken) {
         console.error("Error fetching username from Keycloak:", error);
     }
 
-    return process.env.TRUSTED_USERNAME;
+    return null; // No username should be returned if not valid
 }
 
 module.exports.getUsername = getUsername;
-
