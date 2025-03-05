8545157b922e423396b3624c829c769f4dd0aaa1
Viswam <Saranya.Viswam@gov.bc.ca>
2025-03-04 16:36:50 -0800

    Changes for Redis

--- redisClient.js
+++ redisClient.js
@@ -5,7 +5,8 @@ const client = redis.createClient({
   socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,  // Default Redis port
-  }
+  },
+  password: process.env.REDIS_PASSWORD
 });
 
 client.on("error", (err) => console.error("Redis Error:", err));
