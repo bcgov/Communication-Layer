cc78e81120a3ca3043707744fd38f3e4323697d7
David Okulski <32730627+DavidOkulski@users.noreply.github.com>
2025-08-21 14:22:18 -0700

    removing logs

--- databindingsHandler.js
+++ databindingsHandler.js
@@ -182,7 +182,6 @@ async function readJsonFormApi(datasource, pathParams) {
   }
   try {
     const url = buildUrlWithParams(host, endpoint, pathParams);
-    console.log("URL",url);
     let response;
     const grant =
       await keycloakForSiebel.grantManager.obtainFromClientCredentials();
@@ -202,7 +201,7 @@ async function readJsonFormApi(datasource, pathParams) {
     }
 
     // Store response data
-    console.log("Response:",response);
+    // console.log("Response:",response);
     return ensureObjectOrArray(response.data);
 
   } catch (error) {
