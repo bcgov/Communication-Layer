0fbf1b868c63f3dc77b7c084dac558391af538cc
saranyaviswam <saranya.viswam@gov.bc.ca>
2025-11-05 15:16:13 -0800

    Update interfaces for Caregiver

--- dictionary/interfaces.js
+++ dictionary/interfaces.js
@@ -73,6 +73,62 @@ const interfaces = {
 
 
         ]
+    },
+    CAREGIVER: {
+        interface: [    
+	    {
+        mode: [
+            "portalNew",
+            "portalEdit"
+        ],
+       type: "button",
+      label: "Save",
+      style: "",
+      actions: [
+        {
+          script: "setFormErrors({});if (!validateAllFields()){ setModalTitle(\"Validation Error\"); setModalMessage(\"There are errors in form.The form will be saved in draft form\"); setModalOpen(true); return true; }",
+          action_type: "javascript"
+        },
+        {
+          body: "tokenId: params[\"id\"],savedForm: JSON.stringify(createSavedData())",
+          path: "/application-forms/saveDraft",
+          type: "POST",
+          api_path: "API.saveButtonAction",
+          action_type: "endpoint"
+        },
+        {
+          script: "const isErrorEmpty = Object.keys(formErrors).length === 0;if(!isErrorEmpty){setModalTitle(\"Validation Error\"); setModalMessage(\"There are errors in form.The form will be saved in draft form\"); setModalOpen(true); return true;}else {setModalTitle(\"Success ✅\");setModalMessage(\"Form Saved Successfully as draft.\"); setModalOpen(true);}",
+          action_type: "javascript"
+        }
+      ]
+    },
+	{
+      mode: [
+        "portalNew",
+        "portalEdit"
+      ],
+      type: "button",
+      label: "Complete",
+      style: "",
+      actions: [
+        {
+          script: "setFormErrors({});if (!validateAllFields()){ setModalTitle(\"Validation Error\"); setModalMessage(\"There are errors in form.Please clear them before saving.\"); setModalOpen(true); return false; }",
+          action_type: "javascript"
+        },
+        {
+          body: "tokenId: params[\"id\"],savedForm: JSON.stringify(createSavedData())",
+          path: "/application-forms/submit",
+          type: "POST",
+          api_path: "API.saveButtonAction",
+          action_type: "endpoint"
+        },
+        {
+          script: "setModalTitle(\"Success ✅\");setModalMessage(\"Form Submitted Successfully.\"); setModalOpen(true);await handleSubmit();",
+          action_type: "javascript"
+        }       
+      ]
+    }
+  ]
     }
 };
 
